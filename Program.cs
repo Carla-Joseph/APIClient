612111106cd80b9fecde66888c770d1cf1f464b1
Carla Joseph <carlajseph@gmail.com>
2020-06-02 17:39:33 -0400

    error message

--- Program.cs
+++ Program.cs
@@ -1,12 +1,42 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
 
 namespace APIClient
 {
-  class Program
-  {
-    static void Main(string[] args)
+    class Player
     {
-      Console.WriteLine("Welcome to C#");
+        public int Id { get; set; }
+        public string first_name { get; set; }
+        public string last_name { get; set; }
+        public string position { get; set; }
+        public int height_feet { get; set; }
+        public int height_inches { get; set; }
+        public int weight_pounds { get; set; }
+        // public Team Team { get; set; }
     }
-  }
+    class Response
+    {
+        [JsonPropertyName("data")]
+        public List<Player> players { get; set; }
+    }
+    class Program
+    {
+        static async Task Main(string[] args)
+
+        {
+            var client = new HttpClient();
+
+            var responseAsStream = await client.GetStreamAsync("https://www.balldontlie.io/api/v1/players/");
+
+            var responseWithPlayers = await JsonSerializer.DeserializeAsync<Response>(responseAsStream);
+            var players = responseWithPlayers.players;
+
+            Console.WriteLine(players.Count);
+
+        }
+    }
 }
